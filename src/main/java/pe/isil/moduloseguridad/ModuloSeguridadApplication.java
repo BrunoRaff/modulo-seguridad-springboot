3b92b48e0d8e69df923c4b2a619a9fcd6d6cbd43
NakataModem <bruno.raff1@hotmail.com>
2022-09-22 18:34:19 -0500

    Sesion 04

--- src/main/java/pe/isil/moduloseguridad/ModuloSeguridadApplication.java
+++ src/main/java/pe/isil/moduloseguridad/ModuloSeguridadApplication.java
@@ -3,13 +3,37 @@ package pe.isil.moduloseguridad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class ModuloSeguridadApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ModuloSeguridadApplication.class, args);
 
-		System.out.println("Hola mundo");
+		//Load driver
+
+		Class.forName("com.mysql.jdbc.Driver");
+
+		//Crear Conexion
+		Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/moduloseg",
+				"root",
+				"root");
+
+		//Crear Statement
+		Statement statement = conexion.createStatement();
+		PreparedStatement pt = conexion.prepareStatement("SELECT * FROM USERS WHERE id = ? and username = ?");
+		pt.setInt(1,1);
+		pt.setString(2,"DNI73267854");
+
+		//Ejecutar sentencia
+
+		ResultSet resultado = pt.executeQuery();
+
+		//Recorrer resultado
+		while (resultado.next()){
+			System.out.println(resultado.getString("name"));
+		}
 	}
 
 }
--- src/main/resources/scripts/base_script.sql
+++ src/main/resources/scripts/base_script.sql
@@ -1,5 +1,5 @@
 create table USERS (
-	id int primary key AUTO_ICREMENT not NULL,
+	id int primary key auto_increment not NULL,
 	name varchar(255),
 	lastname varchar(255),
 	username varchar(20),
@@ -10,7 +10,7 @@ create table USERS (
 )
 
 
-//Inserts
+#Inserts
 
 insert into USERS (name,lastname,username,pass,tipDoc,nroDoc,enable)
 ('Jose','Zuniga','DNI73267854','12345','DNI','73267854',1)
@@ -18,15 +18,45 @@ insert into USERS (name,lastname,username,pass,tipDoc,nroDoc,enable)
 insert into USERS (name,lastname,username,pass,tipDoc,nroDoc,enable)VALUES
 ('Maria','Santillan','DNI86789432','12345','DNI','86789432',1)
 
-//UPDATES
+#UPDATES
 
 update USERS set name ='Mariana' where username='DNI86789432';
 
 
-//ELIMINAR
+#ELIMINAR
 
 delete from USERS where USERNAME='DNI86789432';
 
 
-//CONSULTAR
+#CONSULTAR
 select * from USERS
+
+
+
+#PROCEDURES
+
+CREATE PROCEDURE validarLogin ( IN p_username varchar(20),IN p_pass varchar(100),OUT resultado INT)
+	begin
+		SELECT COUNT(*) into resultado FROM USERS
+		WHERE username = p_username AND pass =p_pass;
+	end
+
+
+create procedure listarUserPerId(in p_id INT)
+begin
+
+	select * from USERS where id=p_id;
+end
+
+
+create procedure listarAllUsers()
+begin
+
+	select * from USERS;
+END
+
+
+#Probando store procedure
+
+	CALL validarLogin('DNI86789432','12345', @outResult);
+	select @outResult;
